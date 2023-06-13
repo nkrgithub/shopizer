4f8bfdea62f93829fd8110e2cb3ab4b3f2be584c
qiaoleiatms <qiaolei@microsoft.com>
2023-06-13 22:44:15 +0800

    fix: springfox incompatible with springboot 2.6+

--- sm-shop/src/main/java/com/salesmanager/shop/application/config/DocumentationConfiguration.java
+++ sm-shop/src/main/java/com/salesmanager/shop/application/config/DocumentationConfiguration.java
@@ -4,21 +4,26 @@ import static io.swagger.models.auth.In.HEADER;
 import static java.util.Collections.singletonList;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
+import com.google.common.base.Predicate;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.google.common.base.Predicates;
 
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
@@ -34,10 +39,11 @@ import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2WebMvc
+@EnableSwagger2
 public class DocumentationConfiguration {
 
 	public static final Contact DEFAULT_CONTACT = new Contact("Shopizer", "https://www.shopizer.com", "");
@@ -67,6 +73,7 @@ public class DocumentationConfiguration {
 				.host(HOST)
 				.select()
 				.apis(requestHandlers()).build()
+				.apiInfo(apiInfo())
 				.securitySchemes(Collections.singletonList(new ApiKey("JWT", AUTHORIZATION, HEADER.name())))
 		        .securityContexts(singletonList(
 		            SecurityContext.builder()
@@ -83,13 +90,14 @@ public class DocumentationConfiguration {
 	            .globalResponseMessage(RequestMethod.GET, getMessages);
 
 	}
-	
-	final Predicate<RequestHandler> requestHandlers() {
-		
-		   Predicate<RequestHandler> predicate = RequestHandlerSelectors.basePackage("com.salesmanager.shop.store.api.v1");
-		   predicate = predicate.or(RequestHandlerSelectors.basePackage("com.salesmanager.shop.store.api.v2"));
-		   
-		   return predicate;
+
+	final com.google.common.base.Predicate<RequestHandler> requestHandlers() {
+
+		Set<com.google.common.base.Predicate<RequestHandler>> matchers = new HashSet<Predicate<RequestHandler>>();
+		matchers.add(RequestHandlerSelectors.basePackage("com.salesmanager.shop.store.api.v1"));
+		matchers.add(RequestHandlerSelectors.basePackage("com.salesmanager.shop.store.api.v2"));
+
+		return Predicates.or(matchers);
 
 	}
 
@@ -105,6 +113,39 @@ public class DocumentationConfiguration {
 	private static ArrayList<? extends SecurityScheme> securitySchemes() {
 		return (ArrayList<? extends SecurityScheme>) Stream.of(new ApiKey("Bearer", "Authorization", "header"))
 				.collect(Collectors.toList());
+	}
+
+	@Bean
+	public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+		return new BeanPostProcessor() {
+
+			@Override
+			public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+				if (bean instanceof WebMvcRequestHandlerProvider) {
+					customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+				}
+				return bean;
+			}
+
+			private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+				List<T> copy = mappings.stream()
+					.filter(mapping -> mapping.getPatternParser() == null)
+					.collect(Collectors.toList());
+				mappings.clear();
+				mappings.addAll(copy);
+			}
+
+			@SuppressWarnings("unchecked")
+			private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+				try {
+					Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+					field.setAccessible(true);
+					return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					throw new IllegalStateException(e);
+				}
+			}
+		};
 	}
 
 }
