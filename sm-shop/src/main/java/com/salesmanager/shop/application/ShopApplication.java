307bad12e857c8d0c684adf4c86abe0b24a1278c
小马哥（mercyblitz） <mercyblitz@gmail.com>
2023-06-16 17:36:02 +0800

    Add Eureka

--- sm-shop/src/main/java/com/salesmanager/shop/application/ShopApplication.java
+++ sm-shop/src/main/java/com/salesmanager/shop/application/ShopApplication.java
@@ -4,8 +4,10 @@ import io.microsphere.spring.context.event.ParallelBeanFactoryListener;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ShopApplication {
 
