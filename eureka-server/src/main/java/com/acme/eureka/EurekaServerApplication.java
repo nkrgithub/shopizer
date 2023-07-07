efd7abe5a382854d60df3a685a7b97d8f2b1068c
小马哥（mercyblitz） <mercyblitz@gmail.com>
2023-07-07 14:00:47 +0800

    Update EurekaServerApplication.java

--- eureka-server/src/main/java/com/acme/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/acme/eureka/EurekaServerApplication.java
@@ -19,20 +19,22 @@ package com.acme.eureka;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+
 
 /**
  * Eureka Server
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  */
-@EnableAutoConfiguration
 @EnableEurekaServer
 @EnableScheduling
+@SpringBootApplication
 public class EurekaServerApplication extends SpringBootServletInitializer {
 
     private static final Logger logger = LoggerFactory.getLogger(EurekaServerApplication.class);
