e9b1a0a7887ff33e6ab24eb1f8eebc18be3559f9
小马哥（mercyblitz） <mercyblitz@gmail.com>
2023-08-20 03:04:27 +0800

    Refactor

--- sm-core/src/main/java/com/salesmanager/core/business/configuration/events/AsynchronousEventsConfiguration.java
+++ sm-core/src/main/java/com/salesmanager/core/business/configuration/events/AsynchronousEventsConfiguration.java
@@ -4,23 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.SyncTaskExecutor;
 
 /**
  * Events will be asynchronous (in a different thread)
- * @author carlsamson
  *
+ * @author carlsamson
  */
 @Configuration
 public class AsynchronousEventsConfiguration {
-	
-	   @Bean(name = "applicationEventMulticaster")
-	   public ApplicationEventMulticaster simpleApplicationEventMulticaster() {
-	       SimpleApplicationEventMulticaster eventMulticaster
-	         = new SimpleApplicationEventMulticaster();
-	        
-	       eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
-	       return eventMulticaster;
-	   }
+
+    @Bean(name = "applicationEventMulticaster")
+    public ApplicationEventMulticaster simpleApplicationEventMulticaster() {
+        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
+        eventMulticaster.setTaskExecutor(new SyncTaskExecutor());
+        return eventMulticaster;
+    }
 
 }
